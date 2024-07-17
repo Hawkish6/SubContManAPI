07d30d2de6c0761f38cd7eba3b7ab0368b084e4a
Suranga <ssilva6@hawk.iit.edu>
2024-07-16 22:55:12 -0400

    Create

---
+++ appsettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactManagerApp
+{
+    public static class AppSettings
+    {
+        public static string getConnectionString()
+        {
+            return "Server=DESKTOP-MV6177C; Database = APPDB; User Id=weblogin; Password = Chem1stry;";
+        }
+
+    }
+
+     
+}
